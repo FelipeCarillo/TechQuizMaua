374eba8203c66c59708d538b1fe865874ddbfc89
Felipe Carillo <63021830+FelipeCarillo@users.noreply.github.com>
2023-05-04 00:15:14 -0300

    Delete TechQuiz/Codes directory

--- TechQuiz/Codes/Colors.py
+++
@@ -1,5 +0,0 @@
-
-AzulKing = '#5271FF'
-Branco = 'White'
-Preto = 'black'
-Cinza = "#545454"
--- TechQuiz/Codes/Images.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageTk
-
-
-def Logo():
-    imageLogo = Image.open("Imagens\LOGO.png")
-    imageLogo = ImageTk.PhotoImage(imageLogo)
-    return imageLogo
