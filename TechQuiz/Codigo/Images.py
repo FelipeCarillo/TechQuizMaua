e7fdb7ad47110dcc189b263bef1d2553f339a1be
Felipe Carillo <63021830+FelipeCarillo@users.noreply.github.com>
2023-05-30 23:29:45 -0300

    Ajuste no path das Images.py

--- TechQuiz/Codigo/Images.py
+++ TechQuiz/Codigo/Images.py
@@ -8,41 +8,41 @@ def loadImage(path, resize=None):
         return CTkImage(Image.open(path))
 
 def ImageLogo(resize=None):
-    path = "Imagens/LOGO.png"
+    path = "TechQuizMaua\TechQuiz\imagens\LOGO.png"
     return loadImage(path, resize)
 
 def ImageExcel(resize=None):
-    path = "Imagens/logoExcel.png"
+    path = "TechQuizMaua\TechQuiz\imagens\logoExcel.png"
     return loadImage(path, resize)
 
 def ImageInformation(resize=None):
-    path = "Imagens/Information.png"
+    path = "TechQuizMaua\TechQuiz\imagens\Information.png"
     return loadImage(path, resize)
 
 def ImageCheckEdit(resize=None):
-    path = "Imagens/check.png"
+    path = "TechQuizMaua\TechQuiz\imagens\check.png"
     return loadImage(path, resize)
 
 def ImageEdit(resize=None):
-    path = "Imagens/EDIT.png"
+    path = "TechQuizMaua\TechQuiz\imagens\EDIT.png"
     return loadImage(path, resize)
 
 def ImageRnkgLogo(resize=None):
-    path = "Imagens/RANKING.png"
+    path = "TechQuizMaua\TechQuiz\imagens\RANKING.png"
     return loadImage(path, resize)
 
 def ImageLogoNEscrita(resize=None):
-    path = "Imagens/LogoSemEscrita.png"
+    path = "TechQuizMaua\TechQuiz\imagens\LogoSemEscrita.png"
     return loadImage(path, resize)
 
 def ImageGoBack(resize=None):
-    path = "Imagens/Back.png"
+    path = "TechQuizMaua\TechQuiz\imagens\Back.png"
     return loadImage(path, resize)
 
 def ImageLupa(resize=None):
-    path = "Imagens/lupa.PNG"
+    path = "TechQuizMaua\TechQuiz\imagens\lupa.PNG"
     return loadImage(path, resize)
 
 def ImageGoFoward(resize=None):
-    path = "Imagens/Foward.PNG"
+    path = "TechQuizMaua\TechQuiz\imagens\Foward.PNG"
     return loadImage(path, resize)
